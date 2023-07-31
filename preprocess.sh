1fcdfed01ec4a064eef8143e753036a75a2105bf
Cerber2ol8 <987012302@qq.com>
2023-07-31 20:29:28 +0800

    fix preprocess.sh

--- preprocess.sh
+++ preprocess.sh
@@ -5,7 +5,7 @@ python3 torch2onnx.py
 mkdir onnx_models_opti
 
 python3 -m onnxoptimizer onnx_models/control_net.onnx onnx_models_opti/control_net.onnx
-python3 -m onnxoptimizer onnx_models/vae_decoder.onnx onnx_models_opti/vae.onnx
+python3 -m onnxoptimizer onnx_models/vae_decoder.onnx onnx_models_opti/vae_decoder.onnx
 python3 -m onnxoptimizer onnx_models/unet.onnx onnx_models_opti/unet.onnx
 python3 -m onnxoptimizer onnx_models/FrozenCLIPEmbedder.onnx onnx_models_opti/FrozenCLIPEmbedder.onnx
 
